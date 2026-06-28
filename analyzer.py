e391ba960006e2bf7b48b72118b511aaad5ad7b2
BeskoneDevelopers <ppovgtp@gmail.com>
2026-06-28 08:29:43 +0300

    create cripto analyzer

--- analyzer.py
+++ analyzer.py
@@ -70,7 +70,7 @@ class Analyzer:
         total_cap = sum(coin.get('market_cap', 0) or 0 for coin in self.data)
 
         self.result = {
-            "generate_AT": datetime.now(),
+            "generate_AT": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "total_coins": len(self.data),
             "total_market_cap": total_cap,
             "top_coin": [
@@ -97,3 +97,108 @@ class Analyzer:
         return self.result
 
 
+    def sweet_table(self):
+        #Верхушка топа
+        top_upper_table = Table(
+            title="Топ 3 лидера роста",
+            style="green",
+        )
+        top_upper_table.add_column("Коин", style="cyan")
+        top_upper_table.add_column("Символ", style="blue")
+        top_upper_table.add_column("Изменение за 24ч", style="green")
+
+        for coin in self.result["top_coin"]:
+            change = coin["change_24h"]
+            change_str = f"+{change:.2f}%" if change else "N/A"
+            top_upper_table.add_row(coin['name'], coin['symbol'], change_str)
+
+        self.console.print(top_upper_table)
+
+        #Донные лидеры
+        top_down_table = Table(
+            title="Топ 3 лидера падения",
+            style="red"
+        )
+        top_down_table.add_column("Коин", style="cyan")
+        top_down_table.add_column("Символ", style="yellow")
+        top_down_table.add_column("Изменение за 24ч", style="red")
+
+        for coin in self.result["top_down_coin"]:
+            change = coin["change_24h"]
+            change_str = f"{change:.2f}%" if change else "N/A"
+            top_down_table.add_row(coin['name'], coin['symbol'], change_str)
+
+        self.console.print(top_down_table)
+
+        #Максимальный обьем торгов
+        value = self.result["highest_volume"]
+        self.console.print(Panel(
+            f"[bold]Максимальный объем торгов[/bold]\n"
+            f"Монета: [cyan]{value['name']}[/cyan] ([yellow]{value['symbol']}[/yellow])\n"
+            f"Объем: [green]${value['volume']:,.0f}[/green]",
+            border_style="blue"
+        ))
+
+        #Сумма капитализации
+        total_cap = self.result["total_market_cap"]
+        self.console.print(Panel(
+            f"[bold]Суммарная капитализация топ-50[/bold]\n"
+            f"[green]{total_cap}[/green]",
+            border_style="green"
+        ))
+
+    def save_result(self, filename: str = "Отчет_крипты.json"):
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(
+                self.result,
+                file,
+                indent=4,
+                ensure_ascii=False
+            )
+
+    def run(self):
+        with Progress(
+                SpinnerColumn(),
+                TextColumn("[progress.description]{task.description}"),
+                transient=True,
+        ) as progress:
+
+            # Имитируем процесс загрузки
+            task = progress.add_task("[cyan]Загрузка данных с CoinGecko...", total=None)
+
+            try:
+                self.data = self.featch_market()
+                progress.update(task, description="[green]Данные загружены!")
+                time.sleep(0.5)
+
+            except Exception as e:
+                progress.update(task, description="[red]Ошибка загрузки данных!")
+                self.console.print(f"[red]Ошибка: {e}[/red]")
+                return
+
+            task2 = progress.add_task("[yellow]Анализ данных...", total=None)
+            self.analyze_data()
+            progress.update(task2, description="[green]Анализ завершен!")
+            time.sleep(0.5)
+
+        # Выводим результаты
+        self.sweet_table()
+
+        # Сохраняем отчет
+        self.save_result()
+
+        self.console.print("\n[bold green]Анализ завершен[/bold green]")
+
+
+def main():
+    analyzer = Analyzer()
+    try:
+        analyzer.run()
+    except KeyboardInterrupt:
+        print("\nПрограмма прервана пользователем")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main()
