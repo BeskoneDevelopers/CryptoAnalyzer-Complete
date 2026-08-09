4028100e3e19de1e2e60c145b110b050ae26835a
BeskoneDevelopers <ppovgtp@gmail.com>
2026-08-10 00:11:24 +0500

    Добавление роутеров

--- crypto_analyzer_app/analyzer/urls.py
+++ crypto_analyzer_app/analyzer/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
+
 from rest_framework.routers import DefaultRouter
-from .views import SnapshotViewSet,CoinViewSet, WatchlistViewSet
+from .views import SnapshotViewSet, CoinViewSet, WatchlistViewSet, MarketStatusView, TopMoversView, VolumeTopView
 
 router = DefaultRouter()
 router.register("snapshots", SnapshotViewSet, basename="snapshots")
@@ -7,4 +9,8 @@ router.register("coins", CoinViewSet, basename="coins")
 router.register("watchlist", WatchlistViewSet, basename="watchlist"),
 
 
-urlpatterns = router.urls
+urlpatterns = [
+    patch("analytics/market-stats/", MarketStatusView.as_view(), name="market-stats"),
+    path("analytics/toper-move/", TopMoversView.as_view(), name="toper-move"),
+    path("analytics/volume-top/", VolumeTopView.as_view(), name="volume-top"),
+] + router.urls
