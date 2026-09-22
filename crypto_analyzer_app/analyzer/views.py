fb360c567f6d7fbd4f3ab4c89e63fa163f8d55b4
BeskoneDevelopers <ppovgtp@gmail.com>
2026-09-22 16:38:42 +0500

    refactor: remove duplicated analytics view

--- crypto_analyzer_app/analyzer/views.py
+++ crypto_analyzer_app/analyzer/views.py
@@ -1,16 +1,24 @@
 
-from rest_framework.decorators import action
-from  rest_framework.response import Response
 from django_filters import rest_framework as filters
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from rest_framework.permissions import IsAuthenticated
-
-from .models import Snapshot, Coin, WatchlistItem
-from .serializer import SnapshotSerializer, CoinSerializer, CoinFilter, WatchlistInputSerializer, WatchlistOutputSerializer, CoinPriceAnalyticSerializer
-from .services import remove_from_watchlist, get_market_stats, get_top_movers, get_top_volume
+from .models import Coin, Snapshot, WatchlistItem
+from .serializer import (
+    CoinFilter,
+    CoinPriceAnalyticSerializer,
+    CoinSerializer,
+    SnapshotSerializer,
+    WatchlistInputSerializer,
+    WatchlistOutputSerializer,
+)
+from .services import (
+    get_market_stats,
+    remove_from_watchlist,
+)
 
 
 class SnapshotViewSet(ReadOnlyModelViewSet):
@@ -25,12 +33,12 @@ class CoinViewSet(ReadOnlyModelViewSet):
         .order_by("id")
     )
     serializer_class = CoinSerializer
-    filter_backends = [filters.DjangoFilterBackend]
+    filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = CoinFilter
 
 
 class WatchlistViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated,]
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action in ("create", "delete_watchlist"):
@@ -80,12 +88,3 @@ class TopAnalyticsView(APIView):
         serializer = CoinPriceAnalyticSerializer(data, many=True)
         return Response(serializer.data)
 
-
-class VolumeTopView(APIView):
-    def get(self, request):
-        toper = get_top_volume()
-        if isinstance(toper, dict) and "error" in toper:
-            return Response(toper, status=404)
-
-        serializer = CoinPriceAnalyticSerializer(toper, many=True)
-        return Response(serializer.data)
